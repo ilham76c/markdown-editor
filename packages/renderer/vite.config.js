e474d10a4e42cf0efb9c801c72e521afdce1276f
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-10-31 01:40:37 +0700

    Refactor: add builtinModules in vite.config.js

--- packages/renderer/vite.config.js
+++ packages/renderer/vite.config.js
@@ -3,6 +3,7 @@
 import {chrome} from '../../.electron-vendors.cache.json';
 import {join} from 'path';
 import {renderer} from 'unplugin-auto-expose';
+import { builtinModules } from 'module';
 
 const PACKAGE_ROOT = __dirname;
 
@@ -31,6 +32,9 @@ const config = {
     assetsDir: '.',
     rollupOptions: {
       input: join(PACKAGE_ROOT, 'index.html'),
+      external: [
+        ...builtinModules.filter(m => m !== 'process' || m !== 'assert')
+      ]
     },
     emptyOutDir: true,
     reportCompressedSize: false,
