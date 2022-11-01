9f37b06514814b707730ad0f353caf26c80fce17
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2022-11-01 22:25:58 +0700

    app component

---
+++ packages/renderer/src/app.tsx
@@ -0,0 +1,23 @@
+import React, {useCallback, useState} from 'react';
+import Editor from './editor';
+import Preview from './preview';
+import './app.css';
+
+const App: React.FC = () => {
+  const [doc, setDoc] = useState<string>(`# Hello, World!`);
+  const handleDocChange = useCallback((newDoc: string) => {
+    setDoc(newDoc);
+  }, []);
+
+  return (
+    <div className="app">
+      <Editor
+        onChange={handleDocChange}
+        initialDoc={doc}
+      />
+      <Preview doc={doc} />
+    </div>
+  );
+};
+
+export default App;
